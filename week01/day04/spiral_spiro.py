d2a3090a6d2527ce632a4460dd3106f1b42a2642
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-15 23:23:24 -0700

    Update spiral_spiro.py
    Increased drawing speed and hid the pointer.

--- week01/day04/spiral_spiro.py
+++ week01/day04/spiral_spiro.py
@@ -5,16 +5,16 @@ import turtle
 
 COLORS = ["green", "darkgreen"]
 
-# Set the background color to black and hide the pointer.
+# Set the background color to black.
 turtle.bgcolor("black")
-turtle.hideturtle()
-turtle.speed(500)
 
-
+# Increase the speed and hide the pointer.
 t = turtle.Turtle()
+t.speed(500)
+t.hideturtle()
 
 # From the center, draw lines that are 1 degree off from being a right angle.
-# The lines start at 1 px long and grow with each iteration to be longer.
+# The lines start at 1 px long and grow with each iteration to be 1 px longer.
 # This forms a spiral shape pattern.
 # Also, alternate each line between green and dark-green to emphasize the spiral.
 for i in range(400):
