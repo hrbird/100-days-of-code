b2b62b5086c081cc36f928e21f370eb363c1a67f
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-21 20:36:26 -0700

    Create scoreboard.py

---
+++ week02/snake_game/scoreboard.py
@@ -0,0 +1,31 @@
+# Scoreboard class for the snake game project.
+
+from turtle import Turtle
+
+SCREEN_HEIGHT = 600
+ALIGNMENT = "center"
+FONT = ("Courier New", 20, "bold")
+
+class Scoreboard(Turtle):
+    """A class to keep track of the score 
+    and display it on the screen."""
+    def __init__(self) -> None:
+        super().__init__()
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, SCREEN_HEIGHT/2 - 40)
+
+        self.score = 0
+        self.draw_score()
+
+    def increase_score(self):
+        """Increase the score by 1."""
+        self.score += 1
+        self.draw_score()
+
+    def draw_score(self):
+        """Draw the current score on the screen."""
+        score_str = f"Score: {self.score}"
+        self.clear()
+        self.write(arg=score_str, align=ALIGNMENT, font=FONT)
