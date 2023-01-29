a58352f5bbd0b4e35a89ba0c297b5c2eb1727901
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-28 19:04:15 -0700

    Initial Password Manager files

---
+++ week03/password_manager/main.py
@@ -0,0 +1,24 @@
+# Program that stores usernames and passwords for various sites.
+# It can also generate random secure passwords.
+
+
+from os.path import dirname, join
+import random
+import tkinter as tk
+
+
+#=========================================
+# CONSTANTS
+#=========================================
+
+#=========================================
+# PASSWORD GENERATOR
+#=========================================
+
+#=========================================
+# SAVE PASSWORD
+#=========================================
+
+#=========================================
+# UI SETUP
+#=========================================
