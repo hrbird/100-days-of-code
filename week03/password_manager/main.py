fcb828dd67b36436259302054509adce39582a87
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-28 19:09:13 -0700

    Update main.py
    Created a tkinter window that shows logo.png

--- week03/password_manager/main.py
+++ week03/password_manager/main.py
@@ -11,6 +11,32 @@ import tkinter as tk
 # CONSTANTS
 #=========================================
 
+
+# Color constants
+PINK = "#e2979c"
+RED = "#cc4c35"
+GREEN = "#9ebd75"
+YELLOW = "#ffdf91"
+
+# Font constants
+FONT_NAME = "Courier"
+
+# Canvas size constants
+CANVAS_WIDTH = 200
+CANVAS_HEIGHT = 200
+
+# The number of pixels to pad each widget
+PAD_X = 10
+PAD_Y = 10
+
+# MyPass image file path constants
+CURRENT_DIR = dirname(__file__)
+MYPASS_IMG_FILE_PATH = join(CURRENT_DIR, "./logo.png")
+
+# X and Y positions for items on the canvas
+MYPASS_IMG_X = CANVAS_WIDTH/2
+MYPASS_IMG_Y = CANVAS_HEIGHT/2
+
 #=========================================
 # PASSWORD GENERATOR
 #=========================================
@@ -20,5 +46,26 @@ import tkinter as tk
 #=========================================
 
 #=========================================
-# UI SETUP
+# SET UP UI AND WIDGETS
 #=========================================
+
+# Create a window.
+window = tk.Tk()
+window.title("Pomodoro Timer")
+window.minsize(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+window.config(padx=20, pady=20, bg="white")
+
+# Get the MyPass image.
+mypass_img = tk.PhotoImage(file=MYPASS_IMG_FILE_PATH)
+
+# Add the MyPass image in the center of the window.
+canvas = tk.Canvas(width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white", highlightthickness=0)
+canvas.create_image(MYPASS_IMG_X, MYPASS_IMG_Y, image=mypass_img)
+canvas.grid(row=1, column=1)
+
+#=========================================
+# MAIN LOOP
+#=========================================
+
+# Keep the window and code running.
+window.mainloop()
