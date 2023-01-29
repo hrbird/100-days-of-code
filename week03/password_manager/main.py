f4d8b164b480e3636e6cab2705303a293eb42e8a
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-28 19:56:22 -0700

    Update main.py
    Added functionality to the add_info() function so it saves the data in the text entry fields to a separate text file.

--- week03/password_manager/main.py
+++ week03/password_manager/main.py
@@ -1,22 +1,23 @@
-# Program that stores usernames and passwords for various sites.
-# It can also generate random secure passwords.
+# Program that stores usernames and passwords for various sites. 
 
+# It shows a form for the user to enter a new set of account information: 
+# the name of the website, the email/username, and the password. 
+# There is a button to generate a random secure password, if needed. 
+
+# If the user clicks the Add button at the bottom, this account information 
+# is written to a text file.
 
 from os.path import dirname, join
 import random
 import tkinter as tk
 
-
 #=========================================
 # CONSTANTS
 #=========================================
 
-
 # Color constants
-PINK = "#e2979c"
 RED = "#cc4c35"
 GREEN = "#9ebd75"
-YELLOW = "#ffdf91"
 
 # Font constants
 FONT_NAME = "Arial"
@@ -36,6 +37,9 @@ IPAD_Y = 3
 CURRENT_DIR = dirname(__file__)
 MYPASS_IMG_FILE_PATH = join(CURRENT_DIR, "./logo.png")
 
+# Account data text file path
+DATA_TXT_FILE_PATH = join(CURRENT_DIR, "./data.txt")
+
 # X and Y positions for items on the canvas
 MYPASS_IMG_X = CANVAS_WIDTH/2
 MYPASS_IMG_Y = CANVAS_HEIGHT/2
@@ -54,7 +58,26 @@ def generate_password():
 
 def add_info():
     """Adds the information entered in the form to a text file."""
-    pass
+
+    # Get the data from the entry widgets.
+    website = website_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    # Format the data into a string.
+    data_str = f"{website}, {username}, {password}"
+
+    # Open the data txt file and write the string to it.
+    with open(DATA_TXT_FILE_PATH, "a") as f:
+        f.write(data_str)
+
+    # Clear the text entry forms.
+    website_entry.delete(0, tk.END)
+    username_entry.delete(0, tk.END)
+    password_entry.delete(0, tk.END)
+
+    # Put the cursor back in website_entry.
+    website_entry.focus()
 
 #=========================================
 # SET UP UI AND WIDGETS
@@ -64,7 +87,7 @@ def add_info():
 window = tk.Tk()
 window.title("Pomodoro Timer")
 window.minsize(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=50, pady=50, bg="white")
 
 # Get the MyPass image.
 mypass_img = tk.PhotoImage(file=MYPASS_IMG_FILE_PATH)
@@ -80,6 +103,7 @@ website_label.grid(row=1, column=0)
 
 # Create a website entry box.
 website_entry = tk.Entry(width=50)
+website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 
 # Create an "Email/Username" label.
