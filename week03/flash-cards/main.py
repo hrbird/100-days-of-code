4e55cb3518694ddbd60a962593d860cdb9cf4c7a
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-30 13:59:29 -0700

    Update main.py
    Added functionality so that words the user didn't know or hasn't seen yet are saved to a separate, custom file.

--- week03/flash-cards/main.py
+++ week03/flash-cards/main.py
@@ -1,6 +1,18 @@
-# Program that shows flashcards to study the most common words in Esperanto.
-
-# It shows 
+# A program that shows flashcards to study the most common words 
+# in a different language (in this case, Esperanto). 
+# 
+# It first shows the front of a flashcard with a random word 
+# in the language in the language you are studying. 
+# After 3 seconds, it flips to show the back of the card, 
+# with the translation of the word in English. 
+# 
+# The user can click the green checkmark button if they knew the answer, 
+# or the red X button if they didn't know it.
+#
+# The words that the user didn't know or didn't see yet are saved 
+# to a new file called "words_to_learn.csv". The next time the user 
+# runs this program, this file will be used for the flashcards, 
+# so the user will only be tested on unknown/unfamiliar words.
 
 from os.path import dirname, join
 import random
@@ -51,48 +63,70 @@ LANG_BACK = "English"
 FLIP_TIME = 3000    # Flip the card after 3 seconds
 
 # Language dictionary file path
-DATA_FILE_PATH = join(CURRENT_DIR, "./data/esperanto.csv")
-
 # The data file is a CSV, but the data is separated by semicolons, 
 # not commas, since the English translations include many commas.
-DATA = {}
-try:
-    DATA = pandas.read_csv(DATA_FILE_PATH, sep=';').to_dict(orient="records")
-except FileNotFoundError:
-    print(f"\nError: Could not find data file at {DATA_FILE_PATH}.\n")
+DATA_FILE_PATH = join(CURRENT_DIR, "./data/esperanto.csv")
 
-# Current random word and translation.
+# This file will store the words that the user didn't know or didn't get tested on yet.
+WORDS_TO_LEARN_FILE_PATH = join(CURRENT_DIR, "./data/words_to_learn.csv")
+
+WORDS = {}
+
+# First, try to open words_to_learn.csv, if it has been created.
+try:
+    WORDS = pandas.read_csv(WORDS_TO_LEARN_FILE_PATH, sep=';').to_dict(orient="records")
+except:
+    # Else, open the main data file with the full list of words.
+    try:
+        WORDS = pandas.read_csv(DATA_FILE_PATH, sep=';').to_dict(orient="records")
+    except FileNotFoundError:
+        print(f"\nError: Could not find data file at {DATA_FILE_PATH}.\n")
+    else:
+        print(f"\nGetting word data from file: {DATA_FILE_PATH}")
+else:
+    print(f"\nGetting word data from file: {WORDS_TO_LEARN_FILE_PATH}")
+
+# Current random word and its translation.
 # Making these global variables because it's tricky trying to send parameters 
 # to repeating window.after() calls.
-cur_word = ""
-cur_translation = ""
+cur_word = {}
 
 #=========================================
 # DATA FUNCTIONS
 #=========================================
 
 def get_random_word():
-    """Get a new random word from DATA and show it on the flashcard."""
-    global cur_word, cur_translation
+    """Get a new random word from WORDS and show it on the flashcard."""
+    global cur_word
 
     try:
-        rand_word = random.choice(list(DATA))
-        cur_word = rand_word[LANG_FRONT]
-        cur_translation = rand_word[LANG_BACK]
+        cur_word = random.choice(list(WORDS))
 
-        print(f"\nNew word: {cur_word}, meaning: {cur_translation}")
+        print(f"\nNew word: {cur_word[LANG_FRONT]}, meaning: {cur_word[LANG_BACK]}")
 
         show_word()
 
     except IndexError:
         print(f"\nIndex Error: Could not get a new random {LANG_FRONT} word.")
-        cur_word = ""
-        cur_translation = ""
+        cur_word = {}
 
     except KeyError:
         print(f"\nKey Error: Could not get a new random {LANG_FRONT} word.")
-        cur_word = ""
-        cur_translation = ""
+        cur_word = {}
+
+def remove_word():
+    """If a user gets a word right, remove it from the WORDS dictionary."""
+    WORDS.remove(cur_word)
+
+    # Update the words_to_learn.csv file.
+    try:
+        pandas.DataFrame(WORDS).to_csv(WORDS_TO_LEARN_FILE_PATH, sep=";", index=False)
+    except:
+        print(f"Error: Could not write data to file: {WORDS_TO_LEARN_FILE_PATH}")
+
+    # Go to the next card.
+    get_random_word()
+
 
 #=========================================
 # UI FUNCTIONS
@@ -109,7 +143,7 @@ def show_word():
     # Show the word to learn on the card.
     canvas.itemconfig(flashcard_canvas_img, image=flashcard_front_img)
     canvas.itemconfig(lang_text, text=LANG_FRONT, fill=TEXT_FRONT_COLOR)
-    canvas.itemconfig(word_text, text=cur_word, fill=TEXT_FRONT_COLOR)
+    canvas.itemconfig(word_text, text=cur_word[LANG_FRONT], fill=TEXT_FRONT_COLOR)
 
     # Set a timer to flip the card after a short delay.
     flip_timer = window.after(FLIP_TIME, func=flip_card)
@@ -117,11 +151,11 @@ def show_word():
 
 def flip_card():
     """After a short delay, flip the card and show the English translation."""
-    global cur_translation
+    global cur_word
 
     canvas.itemconfig(flashcard_canvas_img, image=flashcard_back_img)
     canvas.itemconfig(lang_text, text=LANG_BACK, fill=TEXT_BACK_COLOR)
-    canvas.itemconfig(word_text, text=cur_translation, fill=TEXT_BACK_COLOR)
+    canvas.itemconfig(word_text, text=cur_word[LANG_BACK], fill=TEXT_BACK_COLOR)
 
 #=========================================
 # SET UP UI AND WIDGETS
@@ -155,7 +189,7 @@ canvas.grid(row=0, column=0, columnspan=2)
 
 # Create the green checkmark button.
 correct_img = tk.PhotoImage(file=CORRECT_IMG_FILE_PATH)
-correct_button = tk.Button(image=correct_img, highlightthickness=0, command=get_random_word)
+correct_button = tk.Button(image=correct_img, highlightthickness=0, command=remove_word)
 correct_button.grid(row=1, column=0)
 
 # Create the red X button.
