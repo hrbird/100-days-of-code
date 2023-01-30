4bed97974f1f934d0f35341e03ca5313c095b5ac
hrbird <95164465+hrbird@users.noreply.github.com>
2023-01-30 12:55:23 -0700

    Update main.py
    Added functionality to read word data from the CSV and show random words on the flashcard.

--- week03/flash-cards/main.py
+++ week03/flash-cards/main.py
@@ -5,6 +5,7 @@
 from os.path import dirname, join
 import random
 import tkinter as tk
+import pandas
 
 #=========================================
 # CONSTANTS
@@ -12,8 +13,6 @@ import tkinter as tk
 
 # Color constants
 BACKGROUND_COLOR = "#B1DDC6"
-
-# data = read_csv(csv_path, sep=';')
 
 # Canvas size constants
 CANVAS_WIDTH = 800
@@ -48,6 +47,46 @@ LANG_BACK = "English"
 word_front = "bela"
 word_back = "lovely, beautiful"
 
+# Language dictionary file path
+DATA_FILE_PATH = join(CURRENT_DIR, "./data/esperanto.csv")
+
+# The data file is a CSV, but the data is separated by semicolons, 
+# not commas, since the English translations include many commas.
+DATA = {}
+try:
+    DATA = pandas.read_csv(DATA_FILE_PATH, sep=';').to_dict(orient="records")
+except FileNotFoundError:
+    print(f"\nError: Could not find data file at {DATA_FILE_PATH}.\n")
+
+
+#=========================================
+# DATA FUNCTIONS
+#=========================================
+
+def get_random_word():
+    """Get a new random word from DATA and show it on the flashcard."""
+
+    try:
+        rand_word = random.choice(list(DATA))
+        print(f"\nNew word: {rand_word[LANG_FRONT]}, meaning: {rand_word[LANG_BACK]}")
+
+        show_word(rand_word[LANG_FRONT], LANG_FRONT)
+
+    except IndexError:
+        print(f"\nIndex Error: Could not get a new random {LANG_FRONT} word.")
+
+    except KeyError:
+        print(f"\nKey Error: Could not get a new random {LANG_FRONT} word.")
+
+#=========================================
+# UI FUNCTIONS
+#=========================================
+
+def show_word(word, lang):
+    """Show the given word and language on the flashcard."""
+    canvas.itemconfig(lang_text, text=lang)
+    canvas.itemconfig(word_text, text=word)
+
 #=========================================
 # SET UP UI AND WIDGETS
 #=========================================
@@ -68,18 +107,29 @@ canvas.create_image(FLASHCARD_IMG_X, FLASHCARD_IMG_Y, image=flashcard_front_img)
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Write the current language over the card.
-lang_text = canvas.create_text(LANG_LABEL_X, LANG_LABEL_Y, text=LANG_FRONT, fill="black", font=FONT_LANG_LABEL)
+lang_text = canvas.create_text(LANG_LABEL_X, LANG_LABEL_Y, text="", fill="black", font=FONT_LANG_LABEL)
 
 # Write the current word over the card.
-lang_text = canvas.create_text(WORD_LABEL_X, WORD_LABEL_Y, text=word_front, fill="black", font=FONT_WORD_LABEL)
+word_text = canvas.create_text(WORD_LABEL_X, WORD_LABEL_Y, text="", fill="black", font=FONT_WORD_LABEL)
 
 # Add the canvas to the grid.
 canvas.grid(row=0, column=0, columnspan=2)
 
+# Create the green checkmark button.
+correct_img = tk.PhotoImage(file=CORRECT_IMG_FILE_PATH)
+correct_button = tk.Button(image=correct_img, highlightthickness=0, command=get_random_word)
+correct_button.grid(row=1, column=0)
+
+# Create the red X button.
+wrong_img = tk.PhotoImage(file=WRONG_IMG_FILE_PATH)
+wrong_button = tk.Button(image=wrong_img, highlightthickness=0, command=get_random_word)
+wrong_button.grid(row=1, column=1)
 
 #=========================================
 # MAIN LOOP
 #=========================================
+
+get_random_word()
 
 # Keep the window and code running.
 window.mainloop()
