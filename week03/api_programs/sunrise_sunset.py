65d97339f42ddd689b8e8edc5dd9914caad773df
hrbird <95164465+hrbird@users.noreply.github.com>
2023-02-01 16:28:22 -0700

    Update sunrise_sunset.py
    Added a main loop so the user can get data for multiple locations.

--- week03/api_programs/sunrise_sunset.py
+++ week03/api_programs/sunrise_sunset.py
@@ -2,17 +2,9 @@
 # for each solstice and equinox in the current year.
 
 import requests
-import geopy
 from geopy.geocoders import Nominatim
 
-# Get latitude and longitude data for Boise, ID.
-MY_CITY = "Boise, ID"
-geolocator = Nominatim(user_agent="sunrise-sunset")
-location = geolocator.geocode(MY_CITY)
-my_lat = location.latitude
-my_long = location.longitude
-
-def get_sunrise_sunset_times(latitude, longitude):
+def get_sunrise_sunset_times(latitude, longitude, location_name):
     """Get today's sunrise and sunset times for a given location.
 
     Args:
@@ -36,13 +28,36 @@ def get_sunrise_sunset_times(latitude, longitude):
         
         data = response_sun.json()["results"]
 
-        print(f"\nToday's Data for {MY_CITY}:\n")
+        print(f"\nToday's Data for {location_name}:\n")
         print(f"Sunrise:    {data['sunrise']}")
-        print(f"Solar noon: {data['solar_noon']}")
         print(f"Sunset:     {data['sunset']}")
         print(f"Day length: {data['day_length']} hours")
 
     else:
-        print(f"Error getting data for the location ({latitude}, {longitude})...")
+        print(f"Error getting data for the location {location_name}...")
 
-get_sunrise_sunset_times(my_lat, my_long)
+def main():
+    print("Welcome to the Sunrise-Sunset Times program.")
+
+    quit_program = False
+    while not quit_program:
+
+        input_str = input("\nPlease enter a location, or Q to quit:\n> ")
+
+        if input_str.upper() == "Q":
+            print("\nGoodbye!\n")
+            quit_program = True
+
+        else:
+            # Get the lat/long data for the given location.
+            geolocator = Nominatim(user_agent="sunrise-sunset")
+            location = geolocator.geocode(input_str)
+            my_lat = location.latitude
+            my_long = location.longitude
+
+            get_sunrise_sunset_times(my_lat, my_long, location.address)
+
+if __name__ == "__main__":
+    main()
+    
+
